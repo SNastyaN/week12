20038550fe8369c6681448dd87e8d90336ead0b2
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-03-26 21:29:00 +0900

    n

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function nameMy() {
     let MyName = document.getElementById('name').value;
     let small = MyName.toLowerCase();
     document.querySelector('#result').value = small;
-    
-
+}
+function Com(){
+    let text = document.getElementById('comment').value;
+    text = text.replace
 }
